78055dbe6d794960b52c93dc9304bb5555a7ffcc
melvin marrero <melvin-marrero@users.noreply.github.com>
2024-08-07 22:46:41 -0400

    rtr

--- src/compontes/headerConten.jsx
+++ src/compontes/headerConten.jsx
@@ -6,7 +6,7 @@ export default function HeaderConten(){
     return (
         <div className="container header-content"id="contenedor1" >
             <div className="header-tex">
-                <h1>Delicious fasttt food</h1>
+                <h1>Delicious fastt food</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     Reiciendis expedita ea fuga sint, est saepe, dicta quis cum iste, 
                     autem neque fugiat aut a accusamus suscipit eligendi quibusdam? Sapiente, culpa?
